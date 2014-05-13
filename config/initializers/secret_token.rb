8d36c1f290f7971bde96d317b92cb1cf5366bded
Shogo Kawaguchi <platycod0n.ramosa@gmail.com>
2014-05-13 10:15:50 +0900

    rails4初期化

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-OauthTestApp::Application.config.secret_token = '5768660664a283709b8156a6f92131125846e04d0f59f20cb7e23fefd405ae813bfab928f1de07b7faf862e97c38810269df9325be7e219b0c2c6469b76c7d0f'
